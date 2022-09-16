f77a91478ffd89dd83b113e6c96739a4aa3d3bad
Renato Shigueo Fujimoto <98925481+Renatofujimoto@users.noreply.github.com>
2022-09-16 20:12:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-const saida = document.querySelector('.saida')
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $('.navbar').addClass('sticky')
+    } else {
+      $('.navbar').removeClass('sticky')
+    }
+    if (this.scrollY > 500) {
+      $('.scroll-up-btn').addClass('show')
+    } else {
+      $('.scroll-up-btn').removeClass('show')
+    }
+  })
+  $('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 })
+  })
 
-function digitacao(texto, contador) {
-  if (contador < texto.length) {
-    setTimeout(() => {
-      saida.textContent += texto.charAt(contador)
-      contador++
-      digitacao(texto, contador)
-    }, 150)
-  }
-}
-
-digitacao('FRONT - END DEVELOPER', 0)
+  var typed = new Typed('.typing', {
+    strings: ['Front End Developer', 'Web Designer', 'Freelancer'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  })
+  var typed = new Typed('.typing-2', {
+    strings: ['Desenvolvedor Front End', 'Web Designer', 'Freelancer'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  })
+})
