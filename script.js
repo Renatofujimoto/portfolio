ce2efec0c0719755f189c119abeeaf9158463f2e
Renato Shigueo Fujimoto <98925481+Renatofujimoto@users.noreply.github.com>
2022-09-19 00:44:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,15 +16,21 @@ $(document).ready(function () {
   })
 
   var typed = new Typed('.typing', {
-    strings: ['Front End Developer', 'Web Designer'],
+    strings: [' ', 'Front End Developer', 'Web Designer', 'Freelancer'],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
   })
   var typed = new Typed('.typing-2', {
-    strings: ['Desenvolvedor Front End', 'Web Designer'],
+    strings: [' ', 'Front End Developer', 'Web Designer', 'Freelancer'],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
   })
+
+  $('.menu-btn').click(function () {
+    $('.navbar .menu').toggleClass('active')
+    $('.menu-btn i').toggleClass('active')
+  })
 })
+
